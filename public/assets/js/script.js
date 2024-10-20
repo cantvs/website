f31991d589abc9bb9c2ef5d4bbc90b8c0267862f
Joao Martins <martins@ub.uni-koeln.de>
2024-10-20 04:09:03 +0200

    add translation mechanism and sponsors

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -25,4 +25,40 @@ $(document).ready(function () {
     $('[data-toggle="offcanvas"]').click(function () {
           $('#wrapper').toggleClass('toggled');
     });
-  });
+
+});
+
+function getLanguage() {
+  if (localStorage.getItem('language') === null) {
+    setLanguage(defaultLang);
+  }
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'languages/' + localStorage.getItem('language') + '.json', false);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      const lang = JSON.parse(xhr.responseText);
+      language = lang;
+      setLanguageText();
+    }
+  };
+  xhr.send();
+}
+
+function setLanguage(lang) {
+  localStorage.setItem('language', lang);
+  getLanguage();
+}
+
+function setLanguageText() {
+  $('#bio').text(language.bio);
+  $('#concerts_date').text(language.concerts_date);
+  $('#concerts_place').text(language.concerts_place);
+
+  // $('#header_subTitle')[0].innerHTML = language.header_subTitle;
+  // if (!_token) {showLoginMessage();}
+  // // $('#languageBtn')[0].innerHTML = language.languageBtn;
+  // $('#loginUsername')[0].innerHTML = language.loginUsername;
+  // $('#current-user').text(localStorage.getItem('userID'));
+
+}
